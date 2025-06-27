c3646b180e36d7e78691fb5ce78b77f76c08577e
Rakesh <rakeshhniraulaa@gmail.com>
2025-06-27 21:48:10 +0545

    update config keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
 
 map("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "Switch Window left" })
 map("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>", { desc = "Switch Window right" })
